dc510b11402271ecd08e2243e11c32e95d6d01ad
Rodnei Rodrigo <rodneifive34@gmail.com>
2025-08-14 11:12:45 -0300

    Update MUNDO2.py

--- MUNDO2.py
+++ MUNDO2.py
@@ -110,7 +110,8 @@
 
 # DESAFIO 43: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: IMC abaixo de 18,5: Abaixo do Peso – Entre 18,5 e 25: Peso Ideal – 25 até 30: Sobrepeso – 30 até 40: Obesidade – Acima de 40: Obesidade Mórbida
 
-peso = float('Coloque seu peso: ')
-altura = float('Coloque sua altura: ')
-ap = (altura ** 2) / peso
-print(ap)
+peso = float(input('Coloque seu peso: '))
+altura = float(input('Coloque sua altura: '))
+
+imc = peso / (altura ** 2)
+print(f'Seu IMC é {imc:.2f}')
