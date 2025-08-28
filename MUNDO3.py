7b2a12e30e7ba81182587de06cf1c2d090b20eb6
Rodnei Rodrigo <rodneifive34@gmail.com>
2025-08-28 09:45:33 -0300

    Update MUNDO3.py

--- MUNDO3.py
+++ MUNDO3.py
@@ -81,9 +81,11 @@
 
 #DESAFIO 77: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 palavras = ('Programaçao', 'Hello World', 'Python', 'Anoes Pelados')
-
 for p in palavras:
     print(f'\nNa palavra {p} temos ', end=' ')
     for letra in p:
         if letra.lower() in 'aeiou':
             print(letra, end=' ')
+
+
+#DESAFIO 78:
