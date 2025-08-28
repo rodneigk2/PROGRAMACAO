49573699c9a39d3ad46853d5cc8c210981b8bf98
Rodnei Rodrigo <rodneifive34@gmail.com>
2025-08-28 09:45:37 -0300

    Create TESTE.py

---
+++ TESTE.py
@@ -0,0 +1,27 @@
+meses = ('janeiro'
+          'fevereiro'
+             'marco'
+               'abril' 
+                 'maio'
+                  'junho'
+                'julho'
+               'agosto'
+              'setembro'
+            'outubro'
+            'novembro'
+            'dezembro')
+
+for m in range(0, len(meses),1):
+    primeiro = meses[0]
+    ultimo = meses[-1]
+
+
+    escolha_mes = int(input('Escolha um numero de 1 a 12 para ver o mês correspondente: '))
+    
+    break
+
+
+print(m)
+print(f'O primeiro mês é {primeiro} e o ultimo é {ultimo}')    
+    
+
